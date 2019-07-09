d38ad0f6242c33b6d7c7758d55cebf0f579d536d
denys5dev <denys5dev@gmail.com>
2019-07-09 10:15:52 +0200

    backend error

--- apps/backend/src/app/help/help.controller.ts
+++ apps/backend/src/app/help/help.controller.ts
@@ -1,38 +1,49 @@
-import { Controller, Get, Post, Body } from '@nestjs/common';
+import {
+	Controller,
+	Get,
+	Post,
+	Body,
+	HttpException,
+	HttpStatus
+} from '@nestjs/common';
 import { AppService } from '../app.service';
 
 export interface PeriodicElement {
-    name: string;
-    position: number;
-    weight: number;
-    symbol: string;
+	name: string;
+	position: number;
+	weight: number;
+	symbol: string;
 }
 
 const ELEMENT_DATA: PeriodicElement[] = [
-    {position: 1, name: 'Hydrogen', weight: 1.0079, symbol: 'H'},
-    {position: 2, name: 'Helium', weight: 4.0026, symbol: 'He'},
-    {position: 3, name: 'Lithium', weight: 6.941, symbol: 'Li'},
-    {position: 4, name: 'Beryllium', weight: 9.0122, symbol: 'Be'},
-    {position: 5, name: 'Boron', weight: 10.811, symbol: 'B'},
-    {position: 6, name: 'Carbon', weight: 12.0107, symbol: 'C'},
-    {position: 7, name: 'Nitrogen', weight: 14.0067, symbol: 'N'},
-    {position: 8, name: 'Oxygen', weight: 15.9994, symbol: 'O'},
-    {position: 9, name: 'Fluorine', weight: 18.9984, symbol: 'F'},
-    {position: 10, name: 'Neon', weight: 20.1797, symbol: 'Ne'},
-  ];
+	{ position: 1, name: 'Hydrogen', weight: 1.0079, symbol: 'H' },
+	{ position: 2, name: 'Helium', weight: 4.0026, symbol: 'He' },
+	{ position: 3, name: 'Lithium', weight: 6.941, symbol: 'Li' },
+	{ position: 4, name: 'Beryllium', weight: 9.0122, symbol: 'Be' },
+	{ position: 5, name: 'Boron', weight: 10.811, symbol: 'B' },
+	{ position: 6, name: 'Carbon', weight: 12.0107, symbol: 'C' },
+	{ position: 7, name: 'Nitrogen', weight: 14.0067, symbol: 'N' },
+	{ position: 8, name: 'Oxygen', weight: 15.9994, symbol: 'O' },
+	{ position: 9, name: 'Fluorine', weight: 18.9984, symbol: 'F' },
+	{ position: 10, name: 'Neon', weight: 20.1797, symbol: 'Ne' }
+];
 
 @Controller('elements')
 export class HelpController {
-  constructor(private readonly appService: AppService) {}
+	constructor(private readonly appService: AppService) {}
 
-  @Post()
-  create(@Body() element:PeriodicElement) {
-    ELEMENT_DATA.push(element);
-    return ELEMENT_DATA;
-  }
+	@Post()
+	create(@Body() element: PeriodicElement) {
+		const exist = ELEMENT_DATA.find(i => i.name === element.name);
+		if (exist) {
+			throw new HttpException('Already exists', HttpStatus.BAD_REQUEST);
+		}
+		ELEMENT_DATA.push(element);
+		return ELEMENT_DATA;
+	}
 
-  @Get()
-  findAll() {
-    return ELEMENT_DATA;
-  }
+	@Get()
+	findAll() {
+		return ELEMENT_DATA;
+	}
 }
--- apps/frontend/src/app/views/help-page/help-page.component.ts
+++ apps/frontend/src/app/views/help-page/help-page.component.ts
@@ -1,35 +1,64 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import {
+	FormGroup,
+	FormBuilder,
+	Validators,
+	FormControl,
+	FormGroupDirective,
+	NgForm
+} from '@angular/forms';
 import { HelpPageService, PeriodicElement } from './help-page.service';
+import { ErrorStateMatcher } from '@angular/material/core';
+
+/** Error when invalid control is dirty, touched, or submitted. */
+export class MyErrorStateMatcher implements ErrorStateMatcher {
+	isErrorState(
+		control: FormControl | null,
+		form: FormGroupDirective | NgForm | null
+	): boolean {
+		const isSubmitted = form && form.submitted;
+		return !!(
+			control &&
+			control.invalid &&
+			(control.dirty || control.touched || isSubmitted)
+		);
+	}
+}
 
 @Component({
-  selector: 'app-help-page',
-  templateUrl: './help-page.component.html',
-  styleUrls: ['./help-page.component.scss']
+	selector: 'app-help-page',
+	templateUrl: './help-page.component.html',
+	styleUrls: ['./help-page.component.scss']
 })
-
 export class HelpPageComponent implements OnInit {
-  form: FormGroup;
-  dataSource: PeriodicElement[] = [];
-  displayedColumns: string[] = ['position', 'name', 'weight', 'symbol'];
-  constructor(private _fb: FormBuilder, private _helpService: HelpPageService) { }
+	form: FormGroup;
+	dataSource: PeriodicElement[] = [];
+	displayedColumns: string[] = ['position', 'name', 'weight', 'symbol'];
+	matcher = new MyErrorStateMatcher();
+	constructor(
+		private _fb: FormBuilder,
+		private _helpService: HelpPageService
+	) {}
 
-  ngOnInit() {
-    this._helpService.getElements().subscribe(res => {
-      this.dataSource = res;
-    })
-    this.form = this._fb.group({
-      name: [null, Validators.required],
-      weight: [null, Validators.required],
-      symbol: [null, Validators.required],
-      position: [null]
-    })
-  }
+	ngOnInit() {
+		this._helpService.getElements().subscribe(res => {
+			this.dataSource = res;
+		});
+		this.form = this._fb.group({
+			name: [null, Validators.required],
+			weight: [null, Validators.required],
+			symbol: [null, Validators.required],
+			position: [null]
+		});
+	}
 
-  save() {
-      this.form.get('position').setValue(this.dataSource.length + 1);
-      this._helpService.createElement(this.form.value).subscribe(res => {
-        this.dataSource = res;
-      });
-  }
+	save() {
+		console.log('safsadf');
+		this.form.get('position').setValue(this.dataSource.length + 1);
+		if (this.form.valid) {
+			this._helpService.createElement(this.form.value).subscribe(res => {
+				this.dataSource = res;
+			});
+		}
+	}
 }
